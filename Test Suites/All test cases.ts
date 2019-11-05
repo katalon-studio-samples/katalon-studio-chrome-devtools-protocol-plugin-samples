310243ef35a93d72ffedda6df751ba8c7a544d20
Alex <dev.alex.88@gmail.com>
2019-11-05 18:03:08 +0700

    first commit

---
+++ Test Suites/All test cases.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>All test cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a08c5d95-a4d6-4144-b1a6-ed1a373f83c5</testSuiteGuid>
+   <testCaseLink>
+      <guid>0c0afaf6-3a8f-4935-be7f-a7123c583d0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Capture full page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d381b2c1-9920-4712-a5d2-5db979c8ba90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Print PDF</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
